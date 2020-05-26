047b27de560f668ba18fa78a8b3d0c0c968798d9
thomas-coldwell <tcoldwell72@gmail.com>
2020-05-26 23:52:54 +0100

    Fixed typed definition typo

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ImageEditorProps } from './example/image-editor/ImageEditor';
 
-export declare class ImageEditor extends React.Components<ImageEditorProps, {}> {
+export declare class ImageEditor extends React.Component<ImageEditorProps, {}> {
 
   public render(): JSX.Element;
 
