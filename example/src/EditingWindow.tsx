6794a71d5b49fd0cf97c8a9a5c05442627b9f1f7
thomas-coldwell <tcoldwell72@gmail.com>
2020-05-27 14:08:43 +0100

    Added copy src command on build to test example project

---
+++ example/src/EditingWindow.tsx
@@ -0,0 +1,100 @@
+import * as React from 'react';
+import { Image, StyleSheet, LayoutRectangle, View } from 'react-native';
+import { ImageCropOverlay } from './ImageCropOverlay';
+
+interface EditingWindowProps {
+  imageData: {
+    uri: string | undefined;
+    width: number;
+    height: number;
+  };
+  fixedCropAspectRatio: number;
+  imageBounds: {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+  };
+  onUpdateImageBounds: (bounds: any) => void;
+  accumulatedPan: {
+    x: number;
+    y: number;
+  };
+  onUpdateAccumulatedPan: (accumulatedPan: any) => void;
+  cropSize: {
+    width: number;
+    height: number;
+  };
+  onUpdateCropSize: (size: any) => void;
+}
+
+function EditingWindow(props: EditingWindowProps) {
+
+  const [state, setState] = React.useState({
+    initialisedImageBounds: false
+  });
+
+  const { imageData } = props;
+
+  const getImageFrame = async (layout: LayoutRectangle) => {
+    // Find the start point of the photo on the screen and its
+    // width / height from there
+    const editingWindowAspectRatio = layout.height / layout.width;
+    //
+    const imageAspectRatio = imageData.height / imageData.width;
+    let bounds = { x: 0, y: 0, width: 0, height: 0 };
+    let imageScaleFactor = 1;
+    // Check which is larger 
+    if (imageAspectRatio > editingWindowAspectRatio) {
+      // Then x is non-zero, y is zero; calculate x...
+      bounds.x = (((imageAspectRatio - editingWindowAspectRatio) / (imageAspectRatio)) * layout.width) / 2;
+      bounds.width = layout.height / imageAspectRatio;
+      bounds.height = layout.height;
+      imageScaleFactor = imageData.height / layout.height;
+    }
+    else {
+      // Then y is non-zero, x is zero; calculate y...
+      bounds.y = ((1/imageAspectRatio - 1/editingWindowAspectRatio) / (1/imageAspectRatio)) * layout.height / 2;
+      bounds.width = layout.width;
+      bounds.height = layout.width * imageAspectRatio;
+      imageScaleFactor = imageData.width / layout.width;
+    }
+    props.onUpdateImageBounds({
+      imageBounds: bounds,
+      imageScaleFactor
+    });
+    setState({...state, initialisedImageBounds: true});
+  }
+
+  return(
+    <View style={styles.container}>
+      <Image style={styles.image}
+             source={{ uri: imageData.uri }}
+             onLayout={({nativeEvent}) => getImageFrame(nativeEvent.layout)} />
+      {
+        state.initialisedImageBounds ?
+          <ImageCropOverlay imageBounds={props.imageBounds}
+                            fixedAspectRatio={props.fixedCropAspectRatio}
+                            accumulatedPan={props.accumulatedPan}
+                            onUpdateAccumulatedPan={accumulatedPan => props.onUpdateAccumulatedPan(accumulatedPan)}
+                            cropSize={props.cropSize}
+                            onUpdateCropSize={size => props.onUpdateCropSize(size)} />
+        :
+          null
+      }
+    </View>
+  );
+
+}
+
+export { EditingWindow };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  image: {
+    flex: 1,
+    resizeMode: 'contain'
+  }
+})
---
+++ example/src/ImageEditor.tsx
@@ -0,0 +1,140 @@
+import * as React from 'react';
+import { Modal, StyleSheet, View, StatusBar, Alert } from 'react-native';
+import { ControlBar } from './ControlBar';
+import { EditingWindow } from './EditingWindow';
+import { ImageCropOverlay } from './ImageCropOverlay';
+import * as ImageManipulator from 'expo-image-manipulator';
+import { Processing } from './Processing';
+
+export interface ImageEditorProps {
+  visible: boolean;
+  onCloseEditor: () => void;
+  imageData: {
+    uri: string | undefined;
+    width: number;
+    height: number;
+  };
+  fixedCropAspectRatio: number;
+  onEditingComplete: (result: any) => void;
+}
+
+interface ImageEditorStore {
+  imageScaleFactor: number;
+  imageBounds: {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+  },
+  accumulatedPan: {
+    x: number;
+    y: number;
+  },
+  cropSize: {
+    width: number;
+    height: number;
+  },
+  ready: boolean;
+  processing: boolean;
+}
+
+function ImageEditor(props: ImageEditorProps) {
+
+  const initialState = {
+    imageScaleFactor: 1,
+    imageBounds: {
+      x: 0, 
+      y: 0,
+      width: 0,
+      height: 0
+    },
+    accumulatedPan: {
+      x: 0.0,
+      y: 0.0
+    },
+    cropSize: {
+      width: 0,
+      height: 0
+    },
+    ready: false,
+    processing: false
+  };
+
+  const [editorState, setEditorState] = React.useState<ImageEditorStore>(initialState);
+
+  const onPerformCrop = async () => {
+    // Calculate cropping bounds
+    const { imageBounds, accumulatedPan, imageScaleFactor, cropSize } = editorState;
+    const croppingBounds = {
+      originX: Math.round((accumulatedPan.x - imageBounds.x) * imageScaleFactor),
+      originY: Math.round((accumulatedPan.y - imageBounds.y) * imageScaleFactor),
+      width: Math.round(cropSize.width * imageScaleFactor),
+      height: Math.round(cropSize.height * imageScaleFactor)
+    };
+    // Set the editor state to processing and perform the crop
+    setEditorState({...editorState, processing: true});
+    await ImageManipulator.manipulateAsync(props.imageData.uri as string, [
+      { crop: croppingBounds }
+    ])
+    .then(({uri}) => {
+      setEditorState({...editorState, processing: false});
+      props.onEditingComplete({uri});
+      props.onCloseEditor();
+    })
+    .catch((error) => {
+      // If there's an error dismiss the the editor and alert the user
+      setEditorState({...editorState, processing: false});
+      props.onCloseEditor();
+      Alert.alert('An error occurred while editing.');
+    });
+  }
+
+  React.useEffect(() => {
+    // Reset the state of things and only render the UI
+    // when this state has been initialised
+    if (!props.visible) {
+      setEditorState({...initialState, ready: false});
+    }
+    else {
+      setEditorState({...initialState, ready: true});
+    }
+  }, [props.visible]);
+
+  return(
+    <Modal visible={props.visible}
+           transparent>
+      <StatusBar hidden />
+        { 
+          editorState.ready ? 
+            <View style={styles.container}>
+              <ControlBar onPressBack={() => props.onCloseEditor()}
+                          onPerformCrop={() => onPerformCrop()} />
+              <EditingWindow imageData={props.imageData}
+                              fixedCropAspectRatio={1/props.fixedCropAspectRatio}
+                              imageBounds={editorState.imageBounds}
+                              onUpdateImageBounds={bounds => setEditorState({...editorState, ...bounds})}
+                              accumulatedPan={editorState.accumulatedPan}
+                              onUpdateAccumulatedPan={accumulatedPan => setEditorState({...editorState, accumulatedPan: accumulatedPan})}
+                              cropSize={editorState.cropSize}
+                              onUpdateCropSize={size => setEditorState({...editorState, cropSize: size})} />
+            </View>
+          : null 
+        }
+        {
+          editorState.processing ? 
+            <Processing />
+          : null
+        }
+    </Modal>
+  );
+
+}
+
+export { ImageEditor };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#222'
+  }
+})
