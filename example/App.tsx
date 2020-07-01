a5e94e31a67fe2a170610a5236ce933d974bb39d
thomas-coldwell <tcoldwell72@gmail.com>
2020-07-01 15:59:50 +0100

    Updated example UI

--- example/App.tsx
+++ example/App.tsx
@@ -130,7 +130,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    width: '100%',
+    height: '100%',
     justifyContent: 'center',
     alignItems: 'center'
   },
