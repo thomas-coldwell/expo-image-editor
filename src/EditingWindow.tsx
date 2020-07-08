6a0189b90a61f704f21d753018948840a35efb71
thomas-coldwell <tcoldwell72@gmail.com>
2020-07-08 14:58:17 +0100

    Fixed nullified layout issue

--- src/EditingWindow.tsx
+++ src/EditingWindow.tsx
@@ -52,40 +52,43 @@ function EditingWindow(props: EditingWindowProps) {
   }
 
   const onUpdateCropLayout = (layout) => {
-    // Find the start point of the photo on the screen and its
-    // width / height from there
-    const editingWindowAspectRatio = layout.height / layout.width;
-    // 
-    const imageAspectRatio = imageData.height / imageData.width;
-    let bounds = { x: 0, y: 0, width: 0, height: 0 };
-    let imageScaleFactor = 1;
-    // Check which is larger 
-    if (imageAspectRatio > editingWindowAspectRatio) {
-      // Then x is non-zero, y is zero; calculate x...
-      bounds.x = (((imageAspectRatio - editingWindowAspectRatio) / (imageAspectRatio)) * layout.width) / 2;
-      bounds.width = layout.height / imageAspectRatio;
-      bounds.height = layout.height;
-      imageScaleFactor = imageData.height / layout.height;
-    }
-    else {
-      // Then y is non-zero, x is zero; calculate y...
-      bounds.y = ((1/imageAspectRatio - 1/editingWindowAspectRatio) / (1/imageAspectRatio)) * layout.height / 2;
-      bounds.width = layout.width;
-      bounds.height = layout.width * imageAspectRatio;
-      imageScaleFactor = imageData.width / layout.width;
-    }
-    props.onUpdateImageBounds({
-      imageBounds: bounds,
-      imageScaleFactor
-    });
-    setState({
-      ...state, 
-      imageScaleFactor,
-      imageLayout: {
-        height: layout.height,
-        width: layout.width
+    // Check layout is not null
+    if (layout.height != null && layout.width != null) {
+      // Find the start point of the photo on the screen and its
+      // width / height from there
+      const editingWindowAspectRatio = layout.height / layout.width;
+      // 
+      const imageAspectRatio = imageData.height / imageData.width;
+      let bounds = { x: 0, y: 0, width: 0, height: 0 };
+      let imageScaleFactor = 1;
+      // Check which is larger 
+      if (imageAspectRatio > editingWindowAspectRatio) {
+        // Then x is non-zero, y is zero; calculate x...
+        bounds.x = (((imageAspectRatio - editingWindowAspectRatio) / (imageAspectRatio)) * layout.width) / 2;
+        bounds.width = layout.height / imageAspectRatio;
+        bounds.height = layout.height;
+        imageScaleFactor = imageData.height / layout.height;
       }
-    });
+      else {
+        // Then y is non-zero, x is zero; calculate y...
+        bounds.y = ((1/imageAspectRatio - 1/editingWindowAspectRatio) / (1/imageAspectRatio)) * layout.height / 2;
+        bounds.width = layout.width;
+        bounds.height = layout.width * imageAspectRatio;
+        imageScaleFactor = imageData.width / layout.width;
+      }
+      props.onUpdateImageBounds({
+        imageBounds: bounds,
+        imageScaleFactor
+      });
+      setState({
+        ...state, 
+        imageScaleFactor,
+        imageLayout: {
+          height: layout.height,
+          width: layout.width
+        }
+      });
+    }
   }
 
   React.useEffect(() => {
