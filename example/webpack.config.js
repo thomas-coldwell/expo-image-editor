51ac5fdab2456ecd3daf3b972846101f9855bc73
thomas-coldwell <tcoldwell72@gmail.com>
2020-08-17 11:58:31 +0100

    Set up example to use a custom webpack config w/o service workers

---
+++ example/webpack.config.js
@@ -0,0 +1,10 @@
+const createExpoWebpackConfigAsync = require("@expo/webpack-config");
+
+module.exports = async function (env, argv) {
+  const config = await createExpoWebpackConfigAsync(
+    { ...env, offline: false },
+    argv
+  );
+  // Customize the config before returning it.
+  return config;
+};
